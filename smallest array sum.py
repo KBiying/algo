8deb90b109a895c18eeaec4bb704cdc196e982ef
KBiying <jich789@126.com>
2023-09-12 10:54:41 +0800

    feat 9.12

--- smallest array sum.py
+++ smallest array sum.py
@@ -1,3 +1,4 @@
+import sys
 class Solution(object):
     def minSubArrayLen(self, target, nums):
         """
@@ -9,12 +10,24 @@ class Solution(object):
         right = 0
         valid = 0
         sum = 0
+        min_len = sys.maxint
+        # right is useless
         while right < len(nums):
             a = nums[right]
             right += 1
             sum += a
+                
+            print("window[%d, %d)".format(left, right))
 
-            print("window[%d, %d]".format(left, right))
             while sum >= target:
+                
+                temp_len = right - left
+                if temp_len < min_len:
+                    min_len = temp_len
+
                 d = nums[left]
                 left += 1
+                sum -= d
+                
+                
+                
